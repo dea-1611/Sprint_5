93a34dfe44d901cfc0da8a3bb4eecc42ec612965
dea-1611 <dea.ws1611@gmail.com>
2025-07-01 00:34:52 +0500

    Update test_browse_to_personal_account.py

--- tests/test_browse_to_personal_account.py
+++ tests/test_browse_to_personal_account.py
@@ -9,5 +9,5 @@ class TestBrowseToPersonalAccount:
         wait = WebDriverWait(driver, 10)
         wait.until(EC.element_to_be_clickable(Locators.locator_button_personal_account)).click()
 
-        wait.until(EC.url_contains(Links.link_personal_account))
-        assert driver.current_url == Links.link_personal_account
+        wait.until(EC.url_contains(Links.link_personal_account_page))
+        assert driver.current_url == Links.link_personal_account_page
