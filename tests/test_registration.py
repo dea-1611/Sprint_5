2faabad0f499901fb07b9483dd0363ea0556f6c3
dea-1611 <dea.ws1611@gmail.com>
2025-07-01 00:28:17 +0500

    Update test_registration.py

--- tests/test_registration.py
+++ tests/test_registration.py
@@ -6,33 +6,26 @@ from locators import Locators
 
 
 class TestRegistration:
-    self.wait_time = 5
-    self.login_url = Links.link_login_page
-
-    def wait_for_element(self, driver, locator):
-        return WebDriverWait(driver, self.wait_time).until(
-            EC.visibility_of_element_located(locator)
-        )
 
     def test_registration_success(self, driver, test_email, test_password):
         driver.get(Links.link_registration_page)
-        AuthHelper.registration(driver, test_email, test_password)
+        AuthHelper.registration(driver, test_email, test_password, )
         AuthHelper.confirm_registration_success(driver)
-        assert driver.current_url == self.login_url
+        current_url = driver.current_url
+        expected_result = Links.link_login_page
+        assert expected_result == current_url
+
 
     def test_registration_invalid_password(self, driver, test_email):
         driver.get(Links.link_registration_page)
         invalid_pass = "111"
-        name = "Тест"
-
-        # Заполняем форму
-        self.wait_for_element(driver, Locators.locator_name).send_keys(name)
-        self.wait_for_element(driver, Locators.locator_email).send_keys(test_email)
-        self.wait_for_element(driver, Locators.locator_password).send_keys(invalid_pass)
-        self.wait_for_element(driver, Locators.locator_button_registration).click()
-
-        # Проверяем ошибку
-        error_message = self.wait_for_element(
-            driver, TestLocators.locator_message_wrong_password
-        ).text
-        assert error_message == "Некорректный пароль"
+        driver.find_element(*Locators.locator_name).send_keys("Тест")
+        driver.find_element(*Locators.locator_email).send_keys(test_email)
+        driver.find_element(
+            *Locators.locator_password).send_keys(invalid_pass)
+        driver.find_element(*Locators.locator_button_registration).click()
+        error_message = WebDriverWait(driver, 5).until(
+            EC.visibility_of_element_located(
+                Locators. locator_message_wrong_password)).text
+        expected_error_msg = "Некорректный пароль"
+        assert expected_error_msg == error_message
