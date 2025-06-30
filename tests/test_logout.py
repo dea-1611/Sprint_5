5363312e9e5700659ec0be906b1e317969129f29
dea-1611 <dea.ws1611@gmail.com>
2025-07-01 00:27:54 +0500

    Update test_logout.py

--- tests/test_logout.py
+++ tests/test_logout.py
@@ -4,18 +4,14 @@ from links import Links
 from locators import Locators
 
 class TestLogout:
-    self.wait_time = 5
-    self.login_url = Links.link_login_page
-
-    def wait_and_click_logout(self, driver, locator):
-        WebDriverWait(driver, self.wait_time).until(
-            EC.element_to_be_clickable(locator)
-        ).click()
 
     def test_logout_personal_account(self, driver, authorized_user):
-        self.wait_and_click_logout(driver, Locators.locator_button_personal_account)
-        self.wait_and_click_logout(driver, Locators.locator_button_logout)
+        WebDriverWait(driver, 10).until(EC.element_to_be_clickable(
+            Locators.locator_button_personal_account)).click()
+        WebDriverWait(
+            driver, 10).until(
+            EC.element_to_be_clickable(
+                Locators.locator_button_logout)).click()
         WebDriverWait(driver, 10).until(
-            EC.url_to_be(self.login_url)
-        )
-        assert driver.current_url == self.login_url
+            EC.url_to_be(Links.link_login_page))
+        assert driver.current_url == Links.link_login_page
