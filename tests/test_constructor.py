3e4d78b85a6147ed561dc61dd2373430fab71334
dea-1611 <dea.ws1611@gmail.com>
2025-06-30 22:41:48 +0500

    Update test_constructor.py

--- tests/test_constructor.py
+++ tests/test_constructor.py
@@ -24,17 +24,17 @@ class TestBrowseInConstructor:
         assert self.active_class in actual_class
 
     def test_browse_to_buns(self, driver, test_email, test_password):
-        driver.get(Links.main_page_link)
+        driver.get(Links.link_main_page)
         self.wait_and_click_constructor(driver, Locators.locator_constructor_tab_sauce)
         self.wait_and_click_constructor(driver, Locators.locator_constructor_tab_bun)
         self.check_active_tab(driver, Locators.locator_tab_active_bun, 'Булки')
 
     def test_browse_to_sauces(self, driver):
-        driver.get(Links.main_page_link)
+        driver.get(Links.)
         self.wait_and_click_constructor(driver, Locators.locator_constructor_tab_sauce)
         self.check_active_tab(driver, Locators.locator_tab_active_sauce, 'Соусы')
 
     def test_browse_to_fillings(self, driver, test_email, test_password):
-        driver.get(Links.main_page_link)
+        driver.get(Links.link_main_page)
         self.wait_and_click_constructor(driver, Locators.locator_constructor_tab_filling)
         self.check_active_tab(driver, Locators.locator_tab_active_filling, 'Начинки')
